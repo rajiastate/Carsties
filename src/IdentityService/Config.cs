4c8a196627af8037a506226ecd6c5f2cf03f768a
unknown <raj11.iastate@gmail.com>
2024-01-15 15:20:54 -0600

    added gateway

--- src/IdentityService/Config.cs
+++ src/IdentityService/Config.cs
@@ -20,14 +20,26 @@ public static class Config
     public static IEnumerable<Client> Clients =>
         new Client[]
         {
-            new Client
+            new ()
             {
             ClientId="postman",
             ClientName="Postman",
-            AllowedScopes= {"openid","profile","auctionApp"}, 
+            AllowedScopes= {"openid","profile","auctionApp"},
             RedirectUris={"https://postman.com/callback"},
             ClientSecrets=new[]{new Secret("NotASecret".Sha256())},
             AllowedGrantTypes={GrantType.ResourceOwnerPassword}
+            },
+            new ()
+            {
+            ClientId="nextApp",
+            ClientName="nextApp",
+            AllowedScopes= {"openid","profile","auctionApp"},
+            RedirectUris={"https://localhost:3000/api/auth/callback/id-server"},
+            ClientSecrets=new[]{new Secret("secret".Sha256())},
+            AllowedGrantTypes= GrantTypes.CodeAndClientCredentials,
+            RequirePkce = false,
+            AllowOfflineAccess = true,
+            AccessTokenLifetime = 3600*24*30
             }
 
         };
